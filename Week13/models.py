ae5a2c3172a7312aa1d89e3fa7d3dda789ce8dc0
Paris Perdikaris <pgp@seas.upenn.edu>
2020-04-23 16:15:12 -0400

    Lecture #22 slides

--- Week13/models.py
+++ Week13/models.py
@@ -10,34 +10,34 @@ import numpy as np
 
 class PCA:
     # Initialize the class
-    def __init__(self, X, Z_dim):    
+    def __init__(self, X, Z_dim):
         # Normalize
-        self.Xmean, self.Xstd = X.mean(0), X.std(0)
-        X = (X - self.Xmean)/self.Xstd  
-        
+        self.Xmean = X.mean(0)
+        X = X - self.Xmean
+
         self.X = X
         self.N = X.shape[0]
         self.Z_dim = Z_dim
-        
+
         # Sample covariance matrix
         self.S = np.matmul(X.T,X)/(self.N-1)
-        
+
     # Computes the eigendecomposition of the covariance
     def fit(self):
         values, vectors = np.linalg.eig(self.S)
         idx = np.argsort(values)[::-1]
         self.values = values[idx[:self.Z_dim]]
         self.vectors = vectors[:,idx[:self.Z_dim]]
-        
+
     # Encodes data into latent space
     def encode(self, X_star):
-        X_star = (X_star - self.Xmean)/self.Xstd 
+        X_star = X_star - self.Xmean
         Z = np.matmul(self.X, self.vectors)
         return Z
-    
+
     # Decodes latent values back to physical space
     def decode(self, Z):
         X = np.matmul(Z, self.vectors.T)
         # De-normalize
-        X = X*self.Xstd + self.Xmean
+        X = X + self.Xmean
         return X
